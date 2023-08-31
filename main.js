4c2304b5ebbfdccba4db40f7e508af4075f753d2
Luís Felipe Vidotti <125633607+LuVidotti@users.noreply.github.com>
2023-08-30 21:29:38 -0300

    Update main.js

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ form.addEventListener('submit', (e) => {
 })
 
 async function climaTempo(cidade) {
-    const resposta = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cidade}&APPID=92ed7ea1a311d43e789192f2ef5b5389`);
+    const resposta = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&APPID=92ed7ea1a311d43e789192f2ef5b5389`);
     const dados = await resposta.json();
 
     const divDados = document.querySelector('.dados');
